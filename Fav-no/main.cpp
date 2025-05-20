cbc889f74c6f7bd5a63548a24095871f81329f3b
Safal-gg <safal.shrestha355@gmail.com>
2025-05-20 20:52:15 +0545

    Roshan Dai ko Commit message

---
+++ Fav-no/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int fav_num;
+    cout << "whats your favorite number between 1 and 100:" << endl;
+    cin >> fav_num;
+
+    cout << "Your favorite number is:" << fav_num;
+    return 0;
+}
