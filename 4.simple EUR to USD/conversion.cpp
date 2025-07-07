303eab6d6fb6b5760d2e8f93f1825f4e5142e7ab
Safal-gg <safal.shrestha355@gmail.com>
2025-07-07 10:50:30 +0545

    added conversion of euro into usd

---
+++ 4.simple EUR to USD/conversion.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float money;
+    float USD;
+cout<<"Enter money in EUR:";
+cin>>money;
+
+USD=money * 1.18;
+
+cout<<"the money in dollars is:"<<USD<<endl;
+return 0;
+}
